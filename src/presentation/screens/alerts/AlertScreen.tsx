f3ad6a6e9b460befb72c5aaded7a73f9f1a44704
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-05-03 08:17:29 -0400

    Se creó el screem de alertas

--- src/presentation/screens/alerts/AlertScreen.tsx
+++ src/presentation/screens/alerts/AlertScreen.tsx
@@ -1,10 +1,63 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {Alert,Text, View} from 'react-native';
 
-export function AlertScreen() {
+
+import {CustomView} from '../../components/ui/CustomView';
+import {Title} from '../../components/ui/Title';
+import {globalStyles} from '../../../config/theme/theme';
+import {Button} from '../../components/ui/Button';
+
+export const AlertScreen = () => {
+
+  const createTwoButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+
+  const createThreeButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Ask me later',
+        onPress: () => console.log('Ask me later pressed'),
+      },
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'destructive',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+
+    const onShowPrompt = () => {
+
+      Alert.prompt(
+        'Correo electronico',
+        'Enim commodo ut amet esse aliqua.',
+        (valor: string) => console.log({valor}),
+        'secure-text',
+        'Soy el valor por defecto',
+        'number-pad'
+      );
+  
+    };
+
   return (
-    <View>
-      <Text>AlertScreen</Text>
-    </View>
-  )
-}
+    <CustomView style={globalStyles.globalMargin}>
+      <Title safe text="Alertas" />
+
+      <Button text="Alerta - 2 Botones" onPress={createTwoButtonAlert} />
+
+      <View style={{height: 10}} />
+
+      <Button text="Alerta - 3 Botones" onPress={createThreeButtonAlert} />
+
+      <View style={{height: 10}} />
+
+      <Button text="Prompt - Input" onPress={onShowPrompt} />
+    </CustomView>
+  );
+};
