844f63e7732038bb9e7336e5bd8042d826126230
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-05-06 09:03:19 -0400

    Se creó el screem del Infinit Scroll con números

--- src/presentation/screens/ui/InfiniteScrollScreen.tsx
+++ src/presentation/screens/ui/InfiniteScrollScreen.tsx
@@ -1,10 +1,72 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, StatusBar, SafeAreaView, FlatList, ActivityIndicator } from 'react-native'
+import React, { useState } from 'react'
+import { colors } from '../../../config/theme/theme';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+type ItemProps = {title: string};
+
+const Item = ({title}: ItemProps) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 export function InfiniteScrollScreen() {
+
+  const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
+
+  const loadMore = () => {
+    const newArray = Array.from({length: 5}, (_, i) => numbers.length + i);
+
+    setTimeout(() => {
+      setNumbers([...numbers, ...newArray]);
+    }, 3000);
+  };
+
   return (
-    <View>
-      <Text>InfiniteScrollScreen</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={numbers}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.6}
+        renderItem={({item}) => <Item title={item.toString()} />}
+        keyExtractor={item => item.toString()}
+        ListFooterComponent={() => (
+          <View style={{height: 150, justifyContent: 'center'}}>
+            <ActivityIndicator size={40} color={colors.primary} />
+          </View>
+        )}
+      />
+    </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 30,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
