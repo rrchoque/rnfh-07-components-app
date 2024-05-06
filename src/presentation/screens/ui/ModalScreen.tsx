b47757b9fb8583c438e6d49ea4d485568ce57f63
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-05-05 23:42:25 -0400

    Se creó el screem del Modal

--- src/presentation/screens/ui/ModalScreen.tsx
+++ src/presentation/screens/ui/ModalScreen.tsx
@@ -1,10 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {Modal, Platform, Text, View} from 'react-native';
+import {CustomView} from '../../components/ui/CustomView';
+import {Title} from '../../components/ui/Title';
+import {Button} from '../../components/ui/Button';
+import {useState} from 'react';
 
-export function ModalScreen() {
+export const ModalScreen = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
-    <View>
-      <Text>ModalScreen</Text>
-    </View>
-  )
-}
+    <CustomView margin>
+      <Title text="Modal" safe />
+
+      <Button text="Abrir Modal" onPress={() => setIsVisible(true)} />
+
+      <Modal visible={isVisible} animationType="slide">
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'rgba(0,0,0,0.1)',
+          }}>
+          
+          <View style={{paddingHorizontal: 10}}>
+            <Title text="Modal Content" safe />
+          </View>
+
+          <View style={{ flex: 1 }} />
+
+          <Button 
+            text="Cerrar Modal"
+            onPress={ () => setIsVisible(false) }
+            styles={{
+              height: Platform.OS === 'android' ? 40 : 60,
+              borderRadius: 0,
+            }}
+          />
+
+        </View>
+      </Modal>
+    </CustomView>
+  );
+};
