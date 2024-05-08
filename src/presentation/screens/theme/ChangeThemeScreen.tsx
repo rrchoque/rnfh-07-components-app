699a1313afdd66d7e8bcf9b840d31199eead769e
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-05-08 06:04:25 -0400

    Se creó el screem de cambio de theme y los estilos necesarios

--- src/presentation/screens/theme/ChangeThemeScreen.tsx
+++ src/presentation/screens/theme/ChangeThemeScreen.tsx
@@ -1,10 +1,24 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { CustomView } from '../../components/ui/CustomView';
+import { Button, Title } from '../../components';
 
 export const ChangeThemeScreen = () => {
   return (
-    <View>
-      <Text>ChangeThemeScreen</Text>
-    </View>
+    <CustomView margin>
+      <Title text={`Cambiar tema`} safe />
+
+      <Button 
+        text="Light"
+        onPress={() => {}}
+      />
+
+      <View style={{ height: 10 }} />
+
+      <Button 
+        text="Dark"
+        onPress={() => {}}
+      />
+    </CustomView>
   )
 }
